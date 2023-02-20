965da2ee63deca10abde8227c624207763639fa3
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-20 23:43:19 +0500

    dynamic router in react js

---
+++ src/routes/RenderRoute.js
@@ -0,0 +1,9 @@
+const Home = React.lazy(()=>import("../page/Home"))
+
+export const route = [
+    {
+        id:1,
+        path:"/",
+        component:Home
+    }
+]
---
+++ src/routes/index.jsx
@@ -0,0 +1,35 @@
+import React, { Suspense, useMemo } from 'react'
+import { Routes,Route } from 'react-router-dom'
+
+
+import {route} from "./RenderRoute"
+
+
+const RenderRoute = (route)=>{
+    route.map(val=>{
+        val.children && val.children.length ? (
+            <React.Fragment key={val.id}>
+               {<Route path={val.path} element={<val.component/>} />}
+            {RenderRoute(val.children)}
+            </React.Fragment>
+        ):(
+            <Route path={val.path} element={<val.component/>} />
+        )
+    })
+}
+
+
+
+const index = React.memo(() => {
+const RenderRoutes = useMemo(()=>RenderRoute(route),[route])
+
+  return (
+    <Suspense fallback={<h1>Loading....</h1>}> 
+       <Routes>
+        {RenderRoutes}
+       </Routes>
+    </Suspense>
+  )
+})
+
+export default index
